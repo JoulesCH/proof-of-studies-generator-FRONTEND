d23df31fa3f16f46cf7de4c69bff7978c2a0a0da
Julio César Hernández <joules.hdz@gmail.com>
2023-02-05 19:19:32 -0600

    :poop: Navbar and footer need to be improved

---
+++ components/Footer.tsx
@@ -0,0 +1,14 @@
+// simple footer react component with tailwind css
+
+export default function Footer(){
+    return (
+        <footer className="bg-slate-500 h-[26%]">
+            <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+                <div className="relative flex items-center justify-between h-16">
+                    <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
